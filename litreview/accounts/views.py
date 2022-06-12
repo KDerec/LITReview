9f697b9b3de2e1feffeb8989924965e2fd461078
Kévin Dérécusson <derec84@hotmail.fr>
2022-06-12 14:26:09 +0200

    Display followed user and subscriber list

--- litreview/accounts/views.py
+++ litreview/accounts/views.py
@@ -22,11 +22,15 @@ class SignUpView(generic.CreateView):
 
 def subscription(request):
     connected_user = request.user
-    followed_users = UserFollows.objects.all().filter(user=connected_user)
     followed_users_list = []
+    subscriber_list = []
+    followed_users = UserFollows.objects.all().filter(user=connected_user)
+    subscribed_users = UserFollows.objects.all().filter(followed_user=connected_user)
     for user in followed_users:
         followed_users_list.append(user.followed_user)
-    users = (
+    for user in subscribed_users:
+        subscriber_list.append(user.user)
+    followable_users = (
         get_user_model()
         .objects.exclude(username="admin")
         .exclude(username__in=followed_users_list)
@@ -34,11 +38,13 @@ def subscription(request):
         .order_by("username")
     )
     context = {
-        "users": users,
+        "followable_users": followable_users,
+        "followed_users_list": followed_users_list,
+        "subscriber_list": subscriber_list,
     }
     if request.method == "POST":
-        id_selected_user_to_follow = request.POST.get("users-list")
-        user_to_follow = users.filter(id=id_selected_user_to_follow)[0]
+        id_selected_user_to_follow = request.POST.get("followable-users")
+        user_to_follow = followable_users.filter(id=id_selected_user_to_follow)[0]
         create_user_follow(user_to_follow, connected_user)
         return HttpResponseRedirect("")
 
